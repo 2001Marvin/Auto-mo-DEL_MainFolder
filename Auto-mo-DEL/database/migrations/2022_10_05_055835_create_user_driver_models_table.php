bb64dfc6d5688f22cfc34367da78e4cd86e1d21a
rinzler320 <wendeldanmejaran@gmail.com>
2022-10-05 14:53:27 +0800

    made userDriver model, controller and migrated the table and made userDriver.blade (10/5/22-2:52 PM)

---
+++ Auto-mo-DEL/database/migrations/2022_10_05_055835_create_user_driver_models_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_driver_models', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->int('age')->unsigned();
+            $table->enum('gender',['Male','Female']);
+            $table->char('contactNumber',11);
+            $table->binary('license');
+            $table->binary('birthCertificate');
+            $table->string('vehicleType');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_driver_models');
+    }
+};
